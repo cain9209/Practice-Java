61384b167177ed4fe1e9970477aa66922b2b398b
cain9209 <zacharylcain@gmail.com>
2024-08-22 13:51:04 -0500

    Added a practice txt file for testing. Application works, need to add more comments.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        String filePath = "src/FilesForTxtReader/.txt.txt";
 
+        //  instantiate of the object due to contractor in TxtReader
+        TxtReader fileReader = new TxtReader(new File(filePath));
 
-        System.out.println("Hello World");
-        // Your code here
+        // This is where we call the abstracted function from teh Txt Reader class//
+        fileReader.readFile();
+
     }
 
 }
---
+++ src/TxtReader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TxtReader {
+    // want to use private to keep attribute within class //
+    private File file;
+
+    // Set up constructor to instantiate into main with the file object
+    // as a parameter.
+    public TxtReader(File file) {
+        this.file = file;
+    }
+
+    public void readFile() {
+        try {
+            Scanner scanner = new Scanner(file);
+
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not found: " + file.getAbsolutePath());
+            e.printStackTrace();
+        }
+    }
+}
+
+
